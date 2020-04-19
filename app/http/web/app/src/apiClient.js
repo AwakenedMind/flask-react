dd55e0443f76c14e437737337de0dee1b554065d
Robert Arteaga <robertarteaga@roberts-imac.attlocal.net>
2020-04-19 04:42:03 -0700

    added frontend homepage

---
+++ app/http/web/app/src/apiClient.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+const BASE_URI = 'http://localhost:4433'
+
+const client = axios.create({
+    baseURL: BASE_URI,
+    json: true
+})
+
+const APIClient = (accessToken) => {
+
+    const createKudo = (repo) => {
+        return perform('post', '/kudos', repo)
+    }
+
+    const deleteKudo_repo = (repo) => {
+        return perform(`delete', '/kudos/${repo.id}`)
+    }
+
+    const getKudos = () => {
+        perform('get', '/kudos')
+    }
+
+    const perform = async (method, resource, data) => {
+        return client({
+            method,
+            url: resource,
+            data,
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).then(resp => {
+            return resp.data ? resp.data : []
+        })
+    }
+}
+
+export default APIClient
